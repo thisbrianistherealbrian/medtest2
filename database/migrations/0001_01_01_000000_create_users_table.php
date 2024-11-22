604bf15af3fb6739988cf25b5f71e5395f43da50
Whizzy <hey@whizzy.dev>
2024-11-22 18:11:13 +0000

    Create database migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('stripe_customer_id')->nullable()->unique();
+            $table->string('subscription_status')->default('inactive');
+            $table->timestamp('subscription_end')->nullable();
+            $table->integer('max_projects');
+            $table->integer('current_projects_count');
             $table->timestamps();
         });
 
